0fbc33ca1c59ad6d275e63b498dd537b0c98f798
littlexiami <mxlmengxiangling@gmail.com>
2016-04-01 10:31:44 -0400

    The question and the link are in the txt.

--- Two_sum.py
+++ Two_sum.py
@@ -5,16 +5,4 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        tmp=nums
-        num=sorted(nums)
-        l=0
-        r=len(nums)-1
-        while l<r:
-            if num[l]+num[r]<target:
-                print num[l]
-                l+=1
-            elif num[l]+num[r]>target:
-                print num[r]
-                r-=1
-            else:
-                return [tmp.index(num[l]),tmp.index(num[r])]
+        
